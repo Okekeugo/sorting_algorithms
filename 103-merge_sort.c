a3cad374743f69643e8ecc5a579674b511b0d6a6
Okekeugo <chuboyokeke@yahoo.com>
2022-08-16 11:42:10 +0000

    My commit

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,79 +1,89 @@
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * prints - prints left, right, and merged halves
- * @arr: original or tmp array
- * @side: left, right, or merged half
- * @start: starting index
- * @end: ending index
+ * TDMerge - sorts and merges the sub arrays in source
+ * @start: starting index (inclusive) for the left sub array
+ * @middle: end index (exclusive) for the left sub array and
+ * starting index (inclusive) for the right sub array
+ * @end: end index (exclusive) for the right sub array
+ * @dest: destination for data
+ * @source: source of data
+ *
+ * Return: void
  */
-void prints(int *arr, char *side, size_t start, size_t end)
+void TDMerge(size_t start, size_t middle, size_t end, int *dest, int *source)
 {
-	size_t i;
+	size_t i, j, k;
 
-	printf("[%s]: ", side);
-	for (i = start; i < end; i++)
-	{
-		if (i != (end - 1))
-			printf("%d, ", arr[i]);
-		else
-			printf("%d\n", arr[i]);
-	}
-
-}
-/**
- * rec_merge - recursively splits and merges halves
- * @array: original array
- * @sortArr: tmp array to hold sorted elements
- * @l: starting index
- * @r: ending index
- */
-void rec_merge(int *array, int *sortArr, size_t l, size_t r)
-{
-	size_t i, l_half, r_half;
-	size_t mid = (l + r) / 2;
-
-	if (r - l <= 1)
-		return;
-
-	rec_merge(array, sortArr, l, mid);
-	rec_merge(array, sortArr, mid, r);
 	printf("Merging...\n");
-	prints(array, "left", l, mid);
-	prints(array, "right", mid, r);
-	l_half = l;
-	r_half = mid;
-	for (i = l; i < r; i++)
+	printf("[left]: ");
+	print_array(source + start, middle - start);
+	printf("[right]: ");
+	print_array(source + middle, end - middle);
+	i = start;
+	j = middle;
+	for (k = start; k < end; k++)
 	{
-		if ((l_half < mid) &&
-		    ((r_half == r) || (array[l_half] < array[r_half])))
+		if (i < middle && (j >= end || source[i] <= source[j]))
 		{
-			sortArr[i] = array[l_half];
-			l_half++;
+			dest[k] = source[i];
+			i++;
 		}
 		else
 		{
-			sortArr[i] = array[r_half];
-			r_half++;
+			dest[k] = source[j];
+			j++;
 		}
 	}
-	prints(sortArr, "Done", l, r);
-	for (i = l; i < r; i++)
-		array[i] = sortArr[i];
+	printf("[Done]: ");
+	print_array(dest + start, end - start);
 }
+
 /**
- * merge_sort - sorts mergly
- * @array: array sort
- * @size: size of array
+ * TDSplitMerge - recursively splits the array and merges the sorted arrays
+ * @start: starting index (inclusive)
+ * @end: end index (exclusive)
+ * @array: the array to sort
+ * @copy: a copy of the array
+ *
+ * Return: void
+ */
+void TDSplitMerge(size_t start, size_t end, int *array, int *copy)
+{
+	size_t middle;
+
+	if (end - start < 2)
+		return;
+	middle = (start + end) / 2;
+	TDSplitMerge(start, middle, array, copy);
+	TDSplitMerge(middle, end, array, copy);
+	TDMerge(start, middle, end, array, copy);
+	for (middle = start; middle < end; middle++)
+		copy[middle] = array[middle];
+}
+
+/**
+ * merge_sort - sorts an array of integers in ascending order using the
+ * Merge sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: void
  */
 void merge_sort(int *array, size_t size)
 {
-	int *sortArr;
+	size_t i;
+	int *copy;
 
-	if (!(array) || size < 2)
+	if (array == NULL || size < 2)
 		return;
-	sortArr = malloc(sizeof(int) * size);
-	if (!(sortArr))
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
 		return;
-	rec_merge(array, sortArr, 0, size);
-	free(sortArr);
+	for (i = 0; i < size; i++)
+		copy[i] = array[i];
+	TDSplitMerge(0, size, array, copy);
+	free(copy);
 }
